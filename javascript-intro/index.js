ed67cecc2042a3640f0eaf7cc2f150434f305044
angeloluna <angel.luna.najera@gmail.com>
2024-10-03 17:49:35 -0500

    intro javascript

---
+++ javascript-intro/index.js
@@ -0,0 +1,22 @@
+
+let nombre = prompt("nombre");
+let apellido = prompt("apellido")
+let edad = prompt("edad")
+let pelicula = prompt("pelicula")
+
+console.log(nombre)
+console.log(apellido)
+console.log(edad)
+console.log(pelicula)
+
+// console.log("hola tu nombre es angelo, tienes 34 años y tu pelicula favorita es Mi villano favorito")
+
+console.log("hola tu nombre es " + nombre + " tienes " + edad + " años y tu pelicula favorita es " + pelicula)
+
+// console.log("hola tu nombre es ", nombre, " tienes ", edad, " años y tu pelicula favorita es ", pelicula)
+
+console.log(`hola tu nombre es ${nombre}, tienes ${edad} años y tu pelicula favorita es ${pelicula}`)
+
+// "hola tu nombre es angelo, tienes 34 años y tu pelicula favorita es Mi villano favorito"
+
+
